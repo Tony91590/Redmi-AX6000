42d47be9de82d97179ff90c34fdeeef9d27d1274
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-05 19:20:32 +0200

    Remove DTS patch application and related cleanup
    Removed DTS patch application and cleanup of original files.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -166,20 +166,7 @@ EOF
 
 chmod +x files/etc/uci-defaults/99-default-settings
 
-PATCH_DTS="$GITHUB_WORKSPACE/Xiaomi-AX3000T/xiaomi_redmi-router-ax6000-110m-nmbm-dts.patch"
-
-echo "[7] Applying DTS patch..."
-
-patch -p1 < "$PATCH_DTS"
-
-chmod 0644 target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000.dts
-
 rm -f feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js.orig
-rm -f target/linux/mediatek/base-files/lib/preinit/05_set_preinit_iface.orig
-rm -f target/linux/mediatek/filogic/base-files/etc/board.d/02_network.orig
-rm -f target/linux/mediatek/image/filogic.mk.orig
-rm -f package/boot/uboot-tools/uboot-envtools/files/mediatek_filogic.orig
-rm -f target/linux/mediatek/filogic/base-files/etc/board.d/01_leds.orig
 
 mkdir -p target/linux/mediatek/filogic/base-files/etc/hotplug.d/iface
 
