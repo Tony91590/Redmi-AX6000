f4712fd9238fcceabf7f5b5a508e9c76f5ec46df
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-23 10:04:37 +0200

    Remove duplicate file deletion in diy-part2.sh
    Removed duplicate command to delete 01_leds.orig file.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -180,6 +180,7 @@ rm -f target/linux/mediatek/filogic/base-files/etc/board.d/02_network.orig
 rm -f target/linux/mediatek/image/filogic.mk.orig
 rm -f package/boot/uboot-tools/uboot-envtools/files/mediatek_filogic.orig
 rm -f target/linux/mediatek/filogic/base-files/etc/board.d/01_leds.orig
+rm -f target/linux/mediatek/filogic/base-files/etc/board.d/01_leds.orig
 
 mkdir -p target/linux/mediatek/filogic/base-files/etc/hotplug.d/iface
 
