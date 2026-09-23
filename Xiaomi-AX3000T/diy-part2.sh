85fc2486c1b92136c30f096f49d4c502e5d39e36
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-23 11:23:10 +0200

    Clean up diy-part2.sh by removing orig files
    Remove unnecessary original files and clean up the script.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -179,7 +179,7 @@ rm -f target/linux/mediatek/base-files/lib/preinit/05_set_preinit_iface.orig
 rm -f target/linux/mediatek/filogic/base-files/etc/board.d/02_network.orig
 rm -f target/linux/mediatek/image/filogic.mk.orig
 rm -f package/boot/uboot-tools/uboot-envtools/files/mediatek_filogic.orig
-rm -f target/linux/mediatek/filogic/base-files/etc/board.d/01_leds.orig
+rm -f package/kernel/leds-ws2812b/src/leds-ws2812b.c.orig
 rm -f target/linux/mediatek/filogic/base-files/etc/board.d/01_leds.orig
 
 mkdir -p target/linux/mediatek/filogic/base-files/etc/hotplug.d/iface
