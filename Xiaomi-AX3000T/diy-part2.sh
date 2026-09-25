7fce2e0d75094622d96052bf18408e9e796fadf5
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-25 10:10:31 +0200

    Delete odhcpd hotplug script from diy-part2.sh
    Remove hotplug script for odhcpd reload in diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -182,19 +182,4 @@ rm -f package/boot/uboot-tools/uboot-envtools/files/mediatek_filogic.orig
 rm -f package/kernel/leds-ws2812b/src/leds-ws2812b.c.orig
 rm -f target/linux/mediatek/filogic/base-files/etc/board.d/01_leds.orig
 
-mkdir -p target/linux/mediatek/filogic/base-files/etc/hotplug.d/iface
-
-cat > target/linux/mediatek/filogic/base-files/etc/hotplug.d/iface/99-odhcpd-reload <<'ODHCPD_EOF'
-#!/bin/sh
-
-[ "$ACTION" = "ifup" ] || exit 0
-
-if [ "$INTERFACE" = "wan6" ]; then
-        sleep 10
-        /etc/init.d/odhcpd reload
-fi
-ODHCPD_EOF
-
-chmod 0755 target/linux/mediatek/filogic/base-files/etc/hotplug.d/iface/99-odhcpd-reload
-
 echo "Done ✔"
